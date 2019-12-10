94b5dcf29442bf47eef14a75660b5280834ba394
jackiewang <wang_hongke@163.com>
2019-12-10 08:33:40 +0800

    python run in loop

--- scripts/learn_date_run_in_loop.py
+++ scripts/learn_date_run_in_loop.py
@@ -11,12 +11,12 @@ def run_in_loop(start_date, end_date):
         if run_date > end_date:
             print('Game over...')
             break
-        print(run_date)
+        print(run_date)  # this is where the exe function is called
         run_date = (datetime.strptime(start_date, '%Y%m%d') + timedelta(days=start_interval)).strftime('%Y%m%d')
         start_interval = start_interval + 1
 
 
 if __name__ == '__main__':
-    start_date = '20191201'
-    end_date = '20191210'
+    start_date = '20191121'
+    end_date = '20191205'
     run_in_loop(start_date, end_date)
