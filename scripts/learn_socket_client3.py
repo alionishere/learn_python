59d14dcb13c7c7c513b7903721f069c57771f606
jackiewang <wang_hongke@163.com>
2019-12-11 13:49:39 +0800

    python socket client server

--- scripts/learn_socket_client3.py
+++ scripts/learn_socket_client3.py
@@ -6,7 +6,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 9999))
 # 接收欢迎消息:
 print(s.recv(1024).decode('utf-8'))
-for data in [b'Michael', b'Tracy', b'Sarah']:
+for data in [b'Michael3', b'Tracy3', b'Sarah3']:
     # 发送数据:
     print('send: %s' % data)
     s.send(data)
--- scripts/learn_socket_server.py
+++ scripts/learn_socket_server.py
@@ -4,7 +4,12 @@
 import socket
 import threading
 import time
-
+"""
+用TCP协议进行Socket编程在Python中十分简单，对于客户端，
+要主动连接服务器的IP和指定端口，对于服务器，要首先监听指定端口，
+然后，对每一个新的连接，创建一个线程或进程来处理。通常，服务器程序会无限运行下去。
+同一个端口，被一个Socket绑定了以后，就不能被别的Socket绑定了。
+"""
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 监听端口:
 s.bind(('127.0.0.1', 9999))
