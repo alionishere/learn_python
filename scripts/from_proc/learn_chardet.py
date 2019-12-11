1837b18961c768d40e4dd4cc2c91cf1b343f3293
jackiewang <wang_hongke@163.com>
2019-12-11 09:51:56 +0800

    python chardet

--- scripts/from_proc/learn_chardet.py
+++ scripts/from_proc/learn_chardet.py
@@ -2,5 +2,7 @@ import chardet
 
 data = '离离原上草，一岁一枯荣'.encode('gbk')
 data2 = '离离原上草，一岁一枯荣'.encode('utf-8')
+data3 = '最新の主要ニュース'.encode('euc-jp')
 print(chardet.detect(data))
 print(chardet.detect(data2))
+print(chardet.detect(data3))
