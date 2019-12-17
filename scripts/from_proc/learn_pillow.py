0e0c251e9f44f5a466446935cab18e1cd1b435b4
jackiewang <wang_hongke@163.com>
2019-12-17 14:03:19 +0800

    python map reduce

--- scripts/from_proc/learn_pillow.py
+++ scripts/from_proc/learn_pillow.py
@@ -3,4 +3,5 @@ import pytesseract
 
 im = Image.open('1.png')
 # im.show()
-print(pytesseract.image_to_string(Image.open('1.png')))
+# print(pytesseract.image_to_string(Image.open('1.png')))
+print(pytesseract.image_to_string(Image.open('1.png'), lang='chi_sim'))
