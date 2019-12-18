f2d1c92e340f410af0e2e0c40fdc5b45797fd773
jackiewang <wang_hongke@163.com>
2019-12-18 14:32:12 +0800

    python map reduce

---
+++ scripts/learn_socket_spark_client3.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import socket
+import time
+
+
+def gen_test_sql(num):
+    return "insert into cif.t_test_01 values('%s')" % num
+
+
+sql_lst = [gen_test_sql(n) for n in range(3, 10)]
+# sql_lst = [b"insert into cif.t_test_01 values('03')"]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接:
+s.connect(('127.0.0.1', 9998))
+# 接收欢迎消息:
+print(s.recv(4096).decode('utf-8'))
+for data in sql_lst:
+    # 发送数据:
+    print('send: %s' % data)
+    s.send(data)
+    print('accepted: %s' % s.recv(1024).decode('utf-8'))
+    # time.sleep(5)
+s.send(b'exit')
+s.close()
---
+++ scripts/learn_socket_spark_server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# server.py
+import socket
+import threading
+import time
+from pyspark.sql import SparkSession
+
+
+def get_spark(app_name):
+    return SparkSession.builder.master("yarn") \
+        .appName(app_name) \
+        .enableHiveSupport() \
+        .config("spark.sql.parquet.compression.codec", "snappy") \
+        .config("spark.sql.parquet.writeLegacyFormat", "true") \
+        .getOrCreate()
+
+
+def run_sql(spark, sql):
+    spark.sql(sql)
+
+
+def tcplink(sock, addr, spark):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(4096)
+        # time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        run_sql(spark, data)
+        # sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+        sock.send('Hello, success')
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 监听端口:
+s.bind(('127.0.0.1', 9998))
+s.listen(5)
+spark = get_spark('test_001')
+print('Waiting for connection...')
+
+while True:
+    # 接受一个新连接:
+    sock, addr = s.accept()
+    # 创建新线程来处理TCP连接:
+    t = threading.Thread(target=tcplink, args=(sock, addr, spark))
+    t.start()
