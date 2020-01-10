2bb40ef9f782c406ad39ace3e98f65f549b1734f
jackiewang <wang_hongke@163.com>
2020-01-10 10:53:17 +0800

    python map reduce

--- scripts/learn_read_txt.py
+++ scripts/learn_read_txt.py
@@ -1,4 +1,28 @@
 # -*- coding: utf-8 -*-
 
-with open('', 'r') as f:
-    s = f.read()
+lst = []
+st = set()
+with open('doc/test.txt', 'r') as f:
+    # while
+    s = f.readline()
+    # s = s[:-1]
+    while s:
+        lst.append(s)
+        st.add(s)
+        s = f.readline()
+        # s = s[:-1]
+    # print(s)
+
+
+file_no = 1
+for i in st:
+    file_lst = []
+    # print(i, end='')
+    for j in lst:
+        if i == j:
+            file_lst.append(j)
+    print(file_lst)
+    with open('doc/export_%s' % file_no, 'w+') as fw:
+        fw.writelines(file_lst)
+    file_no = file_no + 1
+    print('--' * 10)
