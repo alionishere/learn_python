cf79bfc8cc58744e1ebb01ffaa2ac9ec2e40d3a0
alionishere <wang_hongke@163.com>
2020-06-10 14:43:50 +0800

    cfg.json dbutils.py

--- scripts/learn_word.py
+++ scripts/learn_word.py
@@ -9,40 +9,8 @@ import os
 import time
 
 
-def get_db_conn():
-    db_name = 'postgres'
-    db_user = 'postgres'
-    # pwd = 'postgres'
-    pwd = 'postgres'
-    # host = '127.0.0.1'
-    host = '192.250.107.198'
-    port = '5432'
-    return pg.connect(database=db_name, user=db_user, password=pwd, host=host, port=port)
-
-
-# filename = r'E:\tmp\2020-04-25-601555.SH-601555东吴证券2019年年度报告.doc'
-filename = r"E:\tmp\test1.docx"
-
-# print(os.path.abspath(filename))
-# time.sleep(3)
-doc = Document(filename)
-tables = doc.tables
-
-print(len(tables))
-data = []
-table = tables[142]
-table2 = tables[143]
-
-# 获取表格字段名
-varname = []
-for i in range(len(table.columns)):
-    varname.append(table.cell(0, i).text)
-
-
-# print(varname)
-
-
-def get_df(table):
+def get_df(table, tb_column):
+    data = []
     # 获取表格数据
     for i in range(1, len(table.rows)):
         for j in range(len(table.columns)):
@@ -57,23 +25,63 @@ def get_df(table):
     df = pd.DataFrame(arr2)
     # 给数据集赋予变量名
     # df.columns = ['category','nominal_amount_end_hedging','fair_value_fund_end_hedging','fair_value_debt_end_hedging','nominal_amount_debt_end_unhedging','fair_value_fund_end_unhedging','fair_value_debt_end_unhedging','nominal_amount_start_hedging','fair_value_fund_start_hedging','fair_value_debt_start_hedging','nominal_amount_debt_start_unhedging','fair_value_fund_start_unhedging','fair_value_debt_start_unhedging']
-    df.columns = ['item', 'ending_book_balance_foreign_currency_amount', 'ending_book_balance_conversion_rate',
-                  'ending_book_balance_rmb', 'opening_book_balance_foreign_currency_amount',
-                  'opening_book_balance_conversion_rate', 'opening_book_balance_rmb']
+    df.columns = tb_column
     return df
 
 
-df = get_df(table)
-df = df[1:]
-print(df)
-print('-*' * 30)
-df2 = get_df(table2)
-print(df2)
-# 导出数据
-# df.to_excel(r'E:/tmp/tst_excel.xlsx', index=False)
-
 # 写入数据库
-# db_conn = get_db_conn()
-engine = create_engine('postgresql+psycopg2://postgres:postgres@192.250.107.198/postgres')
-# df.to_sql(name='financial_derivative2', con=engine)
-# df.to_sql(name='t_guaranteed_deposits_paid', con=engine)
+def write2db(df, tb_name):
+    engine = create_engine('postgresql+psycopg2://postgres:postgres@192.250.107.198/postgres')
+    # df.to_sql(name='financial_derivative2', con=engine)
+    df.to_sql(name=tb_name, con=engine, if_exists='replace', index=None)
+
+
+# 获取表格字段名
+def get_tb_varname(table):
+    varname = []
+    for i in range(len(table.columns)):
+        varname.append(table.cell(0, i).text)
+    return varname
+
+
+if __name__ == '__main__':
+    # filename = r'E:\tmp\2020-04-25-601555.SH-601555东吴证券2019年年度报告.doc'
+    filename = r"E:\tmp\test1.docx"
+
+    doc = Document(filename)
+    tables = doc.tables
+    print(len(tables))
+
+    ###########################
+    # 存出保证金              #
+    ###########################
+    table = tables[142]
+    table2 = tables[143]
+    tb_column = ['item', 'ending_book_balance_foreign_currency_amount', 'ending_book_balance_conversion_rate',
+                  'ending_book_balance_rmb', 'opening_book_balance_foreign_currency_amount',
+                  'opening_book_balance_conversion_rate', 'opening_book_balance_rmb']
+    df = get_df(table, tb_column)
+    df = df[1:]
+    df2 = get_df(table2, tb_column)
+    df_concat = pd.concat([df, df2])
+    # print(df_concat.iloc[1:])
+    # 写入数据库
+    write2db(df_concat, 't_guaranteed_deposits_paid')
+
+    ###########################
+    # 资本公积 P: 193         #
+    ###########################
+    table = tables[207]
+    tb_column = ['item', 'initial_balance', 'current_increase', 'current_decrease', 'closing_balance']
+    df = get_df(table, tb_column)
+    print(df)
+    write2db(df, 't_capital_reserve')
+
+    ###########################
+    # 库存股   P: 193         #
+    ###########################
+    table = tables[208]
+    tb_column = ['item', 'initial_balance', 'current_increase', 'current_decrease', 'closing_balance']
+    df = get_df(table, tb_column)
+    print(df)
+    write2db(df, 't_treasure_stock')
