ed7e4ff3823b9a753d0a76656c2da1f071564c12
alionishere <wang_hongke@163.com>
2020-12-25 18:03:00 +0800

    cfg.json dbutils.py

--- scripts/learn_pdf_adaptor.py
+++ scripts/learn_pdf_adaptor.py
@@ -10,7 +10,8 @@ dic = ['资产负债率（%）', '资产总计', '负债合计', '其中：客�
 dic1 = ['融出资金', '买入返售金融资产', '应收利息', '交易性金融资产', '其他债权投资', '长期股权投资', '应付债券', '应付短期融资款', '交易性金融资产', '衍生金融资产']
 dic2 = ['手续费及佣金净收入', '一、营业总收入', '利息净收入', '投资收益（损失以“-”号填列）', '公允价值变动收益（损失以“-”号填列）', '二、营业总支出',
         '归属于母公司股东的净利润', '五、净利润（净亏损以“－”号填列）']
-dic3 = ['其中：对联营企业和合营企业的投资收益', '一、营业总收入', '投资收益（损失以“-”号填列）', '公允价值变动收益（损失以“-”号填列）', '二、营业总支出 ']
+dic3 = ['其中：对联营企业和合营企业的投资收益', '一、营业总收入', '投资收益（损失以“-”号填列）', '公允价值变动收益（损失以“-”号填列）', '二、营业总支出',
+        '3.可供出售金融资产公允价值变动损\n益', '2.其他债权投资公允价值变动']
 dic4 = ['净资本', '风险覆盖率（%）', '资本杠杆率（%）', '流动性覆盖率（%）', '净稳定资金率（%）']
 
 k = 0
@@ -19,7 +20,6 @@ k = 0
 pdf = pdfplumber.open('E:/tmp/dwzq.pdf')
 
 print('开始读取数据')
-print('\n')
 
 for page in pdf.pages[1:300]:
     data = page.extract_text()
@@ -31,7 +31,6 @@ for page in pdf.pages[1:300]:
         for i in range(page_number - 1, page_number + 2):
             page_use = pdf.pages[i]
             for table in page_use.extract_tables():
-
                 for row in table:
                     for m in dic1:
                         if m in row:
@@ -73,6 +72,7 @@ for page in pdf.pages[1:300]:
             for table in page_use.extract_tables():
 
                 for row in table:
+                    # print(row)
                     for m in dic3:
                         if m in row:
                             table1.append(row)
