cccc5c0006c8ddf653fc3899602588b32789833a
alionishere <wang_hongke@163.com>
2021-01-19 18:57:11 +0800

    cfg.json dbutils.py

--- scripts/learn_pdf_adaptor_v4.py
+++ scripts/learn_pdf_adaptor_v4.py
@@ -26,18 +26,20 @@ dic5 = ['华泰证券']
 # 方案2
 # 通过表名+页码定位
 # 表名#表序号#是否跨页#页码&表定位
-ebs = ['结算备付金#1#n#129&1', '融出资金#1#n#129&2', '融出资金#2#y#129&3&1', '衍生金融工具#1#y#130&2&2', '买入返售金融资产#1#y#133&2&1',
-       '买入返售金融资产#2#n#134&2', '买入返售金融资产#3#n#134&3', '存出保证金#1#n#131&1']
+ebs = ['结算备付金#1#n#0#129&1', '融出资金#1#n#0#129&2', '融出资金#2#y#1#129&3&1', '衍生金融工具#1#y#1#130&2&2', '买入返售金融资产#1#y#1#133&2&1',
+       '买入返售金融资产#2#n#0#134&2', '买入返售金融资产#3#n#0#134&3', '存出保证金#1#n#0#131&1', '交易性金融资产#1#n#0#136&1',
+       '交易性金融资产#2#n#0#137&3', '交易性金融资产#3#n#0#137&2', '债权投资#1#y#1#137&1&2', '债权投资#2#n#0#138&3', '其他债权投资#1#n#0#138&1',
+       '其他权益工具投资#1#n#0#139&1', '其他权益工具投资#1#n#0#140&2', '长期股权投资#1#y#2#140&1&1']
 # ebs = ['2、结算备付金#1#n#129&1']
 
 pdf = pdfplumber.open(r'E:\tmp\光大证券2019年年度报告.pdf')
 
-# 母公司资产负债表
 for i in ebs:
     item = i.split('#')[0]
     item_no = int(i.split('#')[1])  # 指标项序号
     double_spread = i.split('#')[2].lower().strip()  # 是否跨页：y 跨页， n 不跨页
-    detail_cfg = i.split('#')[3]  # 若跨页，则配置第几页，否则，配置第几页第几个表
+    spread_no = int(i.split('#')[3])  # 若跨页，跨页的次数，若不跨页，默认为0
+    detail_cfg = i.split('#')[4]  # 若跨页，则配置第几页，否则，配置第几页第几个表
     item_page_no = int(detail_cfg.split('&')[0])
     tbls = pdf.pages[item_page_no - 1].extract_tables()
     tbl_name = 'item: %s_%s' % (item, item_no)
@@ -50,19 +52,37 @@ for i in ebs:
             print(row_lst)
         print('-*' * 50)
     elif double_spread == 'y':
-        item_tb_no_1 = int(detail_cfg.split('&')[1])
-        item_tb_no_2 = int(detail_cfg.split('&')[2])
-        tbls_2 = pdf.pages[item_page_no].extract_tables()
-        tgt_tb_1 = tbls[item_tb_no_1 - 1]
-        tgt_tb_2 = tbls_2[item_tb_no_2 - 1]
-        print('.....%s' % len(tbls_2))
-        for row in tgt_tb_1:
-            row_lst = [n for n in row]
-            print(row_lst)
-        for row in tgt_tb_2:
-            row_lst = [n for n in row]
-            print(row_lst)
-        print('-*' * 50)
+        if spread_no <= 1:
+            item_tb_no_1 = int(detail_cfg.split('&')[1])
+            item_tb_no_2 = int(detail_cfg.split('&')[2])
+            tbls_2 = pdf.pages[item_page_no].extract_tables()
+            tgt_tb_1 = tbls[item_tb_no_1 - 1]
+            tgt_tb_2 = tbls_2[item_tb_no_2 - 1]
+            for row in tgt_tb_1:
+                row_lst = [n for n in row]
+                print(row_lst)
+            for row in tgt_tb_2:
+                row_lst = [n for n in row]
+                print(row_lst)
+            print('-*' * 50)
+        else:
+            item_tb_no_1 = int(detail_cfg.split('&')[1])
+            tgt_tb_1 = tbls[item_tb_no_1 - 1]
+            for row in tgt_tb_1:
+                row_lst = [n for n in row]
+                print(row_lst)
+            for m in range(item_page_no, item_page_no + spread_no):
+                tgt_tbl_m = pdf.pages[item_page_no].extract_tables()[0]
+                for row in tgt_tbl_m:
+                    row_lst = [n for n in row]
+                    print(row_lst)
+            tbls_2 = pdf.pages[item_page_no + spread_no - 1].extract_tables()
+            item_tb_no_2 = int(detail_cfg.split('&')[2])
+            tgt_tb_2 = tbls_2[item_tb_no_2 - 1]
+            for row in tgt_tb_2:
+                row_lst = [n for n in row]
+                print(row_lst)
+            print('-*' * 50)
         # for row in tbls_2[1]:
         #     row_lst = [n for n in row]
         #     print(row_lst)
