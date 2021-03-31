46b2d8740d1cbdfc294eceb2beee95f51819f8bb
alionishere <wang_hongke@163.com>
2021-03-31 15:47:00 +0800

    cfg.json dbutils.py

--- scripts/learn_camelot.py
+++ scripts/learn_camelot.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import camelot
+import time
 import pdfplumber
 import pandas as pd
 
-path = r'E:\tmp\东北证券：2019年年度报告.pdf'
+path = r'E:\tmp\中信证券：中信证券股份有限公司2020年年度报告.pdf'
 # extract tb from pdf
-tbs = camelot.read_pdf(path, pages='149-175', flavor='stream')
+tb_page = 190
+start_time = time.time()
+tbs = camelot.read_pdf(path, edge_tol=500, pages=str(tb_page), flavor='stream')
 # tbs = camelot.read_pdf(path)
 print(len(tbs))
 print(tbs[0].data)
+end_time = time.time()
+print(end_time - start_time)
 # with pdfplumber.open(path) as pdf:
 #     # content = ''
 #     # for i in range(len(pdf.pages)):
