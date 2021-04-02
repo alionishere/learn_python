88bd2a15614d63e319add71d6b7df6815e91a2da
alionishere <wang_hongke@163.com>
2021-04-02 14:21:46 +0800

    cfg.json dbutils.py

--- scripts/learn_oracle.py
+++ scripts/learn_oracle.py
@@ -1,31 +1,32 @@
 import cx_Oracle
 
 # ----------------Oracle demo-------------------#
-o_user = 'dcp'
-o_pwd = 'dcp'
-o_ip = '192.250.107.199'
+o_user = 'fxgl_data'
+o_pwd = 'fxgl_data'
+# o_ip = '192.250.107.199'
+o_ip = '192.250.107.142'
 o_port = '1521'
-o_instance = 'sjgk'
+o_instance = 'fkgl'
 
-sql = 'select count(1) from all_all_tables'
-sql1 = 'SELECT * FROM dept '
+sql = 'insert into fxgl.t_test values(:1, :2, :3, :4)'
+data_lst = ['1', 'jack', '001', '002']
 
-o_db = cx_Oracle.connect(o_user, o_pwd, o_ip + ':' + o_port + '/' + o_instance)
-o_cur = o_db.cursor()
-o_cur.execute(sql)
-
-titles = [i[0] for i in o_cur.description]
-sql_set = o_cur.fetchall()
-
-for title in titles:
-    print(title, end='\t\t')
-
-print('')
-
-for sql_line in sql_set:
-    for line in sql_line:
-        print(line, end='\t\t')
-    print('')
+o_conn = cx_Oracle.connect(o_user, o_pwd, o_ip + ':' + o_port + '/' + o_instance)
+o_cur = o_conn.cursor()
+o_cur.execute(sql, data_lst)
+o_conn.commit()
+# titles = [i[0] for i in o_cur.description]
+# sql_set = o_cur.fetchall()
+#
+# for title in titles:
+#     print(title, end='\t\t')
+#
+# print('')
+#
+# for sql_line in sql_set:
+#     for line in sql_line:
+#         print(line, end='\t\t')
+#     print('')
 
 o_cur.close()
-o_db.close()
+o_conn.close()
