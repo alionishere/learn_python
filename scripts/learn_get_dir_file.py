005448e383e4b24bc07791fba974368ab7b6f5a6
alionishere <wang_hongke@163.com>
2021-06-16 11:10:37 +0800

    cfg.json dbutils.py

--- scripts/learn_get_dir_file.py
+++ scripts/learn_get_dir_file.py
@@ -9,5 +9,15 @@ def file_name(file_dir):
         # print(files)  # 当前路径下所有非目录子文件
 
 
+# 只获取当前路径下文件名,不获取文件夹中文件名
+def file_name(file_dir):
+    L=[]
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if os.path.splitext(file)[1] == '.jpeg':  # 想要保存的文件格式
+                L.append(os.path.join(root, file))
+    return L
+
+
 file_path = r'\\10.3.1.138\movie'
 file_name(file_path)
